9fa7b1255a2954701d0e9f512fbb022e62a423ba
Arseniy <punkoarseniy@gmail.com>
2023-05-01 19:18:57 +0300

    Make changes to the environment

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,10 @@
+const tailwindcss = require('tailwindcss');
 module.exports = ({ env }) => ({
     parser: false,
     plugins: {
         'postcss-import': {},
         'postcss-cssnext': {},
         cssnano: env === 'production' ? {} : false,
+        tailwindcss,
     },
 });
